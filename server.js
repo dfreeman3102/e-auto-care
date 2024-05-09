0c9aff9b68d4cb54532ad58ff09eda88cb143a4b
ianhara <ianhara97@gmail.com>
2024-05-09 19:54:54 -0400

    set up server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require('./routes');
+
+const app = express();
+app.use(bodyParser.json());
+
+// Define routes
+app.use('/', routes);
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
