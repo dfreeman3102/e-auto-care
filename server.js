522e74676a3802aee142cb85b93663f59eeea373
ianhara <ianhara97@gmail.com>
2024-05-09 20:59:09 -0400

    implemented sessions with middleware and syncing DB on server start

--- server.js
+++ server.js
@@ -1,15 +1,35 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const routes = require('./routes');
+const session = require('express-session')
+const routes = require('./controllers');
+const sequelize = require('./config/connection.js')
+
 
 const app = express();
-app.use(bodyParser.json());
+const PORT = process.env.PORT || 3001;
 
-// Define routes
+//set up session
+const sess = {
+    secret: 'Super secret secret',
+    cookie:{
+        maxAge: 3600000,
+        secure: false 
+    },
+    resave:false,
+    saveUninitalized: true,
+    store: new session.MemoryStore()
+};
+
+//session middleware
+app.use(session(sess));
+
+//json and url middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// use routes in controllers
 app.use('/', routes);
 
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// sync data Start the server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+  });
