4b355f75044ca5a1047240685056c1ca9d4a3343
dfreeman3102 <davidfreeman3102@gmail.com>
2024-05-13 20:06:48 -0400

    changed the controllers layout

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-//route handling
-const appintmentRoutes = require('./appointmentRoutes');
-const authRoutes = require('./authRoutes');
-const userRoutes = require('./userRoutes');
-
-module.exports = router;
