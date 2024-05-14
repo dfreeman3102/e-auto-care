9eb9cf2229ae265d8f00308214e29bf5e1df758f
dfreeman3102 <davidfreeman3102@gmail.com>
2024-05-13 21:22:59 -0400

    added logic for sign up form

--- public/js/login.js
+++ public/js/login.js
@@ -21,4 +21,31 @@ const loginHandler = async (event) => {
     }
 };
 
+const signupHandler = async (event) => {
+    event.preventDefault();
+
+    //gets the data from the proper fields
+    const userName = document.querySelector('#username-signup');
+    const firstName = document.querySelector('#first-name-signup');
+    const lastName = document.querySelector('#last-name-signup');
+    const email = document.querySelector('#email-signup');
+    const password = document.querySelector('#password-signup');
+
+    if(userName && firstName && lastName && email && password) {
+        const response = await fetch('/api/users', {
+            method:'POST',
+            body: JSON.stringify({firstName, lastName, email, password}),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if(response.ok){
+            document.location.replace('/');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
 document.querySelector('.login-form').addEventListener('submit', loginHandler);
+
+document.querySelector('.signup-form').addEventListener('submit', signupHandler);
