ff6d35bad8fdc32ef85fd8fcd7a055fa9276f73d
Ralph Molu <rmolu11@gmail.com>
2024-05-14 21:09:49 -0400

    added s service model, defined relatiomships, and edited appointment models, and added service seed data

---
+++ seeds/seedServices.js
@@ -0,0 +1,22 @@
+const Service = require('../models/serviceModel');
+
+const servicesData = [
+    'Oil Change',
+    'Tire Rotations',
+    'Brake Fluid Flush',
+    'Head Light Replacement',
+    'Tire Repair',
+    'Transmission Service',
+    'Ceramic Coating',
+    'Window Tinting',
+    'Brake Replacement',
+    'Caliper Replacement',
+    'A/C Service'
+];
+
+const seedServices = async () => {
+    await Service.bulkCreate(servicesData);
+};
+
+module.exports = serviceData;
+
